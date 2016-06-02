06d6d21fef392c4169c42cdb2b1771dffd9ff0f5
fepz <franpaez@gmail.com>
2016-06-02 09:24:42 -0300

    Use corrected deadline if present in the schedubility test.

--- rtsutils.py
+++ rtsutils.py
@@ -5,7 +5,7 @@ def verify_with_empty_slot(tasks):
         summation = 0
         for task in tasks:
             summation += (task["c"] /task["t"])
-        return summation < 1
+        return summation <= 1
 
     def get_workload(t, tasks):
         workload = 0
@@ -28,7 +28,7 @@ def verify_with_empty_slot(tasks):
     for i, task in enumerate(tasks[1:], 1):
         empty_slot = get_last_empty_slot(task, tasks[:i])
         empty_slots.append(empty_slot)
-        schedulable = verify_uf_sum(tasks[:i]) and (task["d"] >= empty_slot)
+        schedulable = task["new_d"] >= empty_slot if "new_d" in task else task["d"] >= empty_slot
         if not schedulable:
             break
 
